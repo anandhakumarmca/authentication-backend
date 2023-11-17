e5aeb7c7ea08b42a9de807ae3656cd8932f1e9f2
Anandh Kumar <accetanandhmca@gmail.com>
2023-11-17 12:48:22 +0530

    changes done

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/",async(req,res)=>{
     try{
         const auth_token = await req.headers.authorization
-        const loginCredentials=AuthorizeUser(token)
+        const loginCredentials=AuthorizeUser(auth_token)
         if(loginCredentials === false){
             res.status(200).send("Invalid Token")
         }else{
