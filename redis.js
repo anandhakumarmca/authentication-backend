af4fdca14d266d27828e641a8c230dfa15765dbb
Anandh Kumar <accetanandhmca@gmail.com>
2023-11-17 20:16:09 +0530

    changes updated

--- redis.js
+++ redis.js
@@ -1,30 +1,39 @@
 const redis = require("redis");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 
+const redisClient = () => {
+  const redisUrl = process.env.redis_url;
 
-const redisClient = ()=>{
-    return  redis.createClient(
-        {
-            url:process.env.redis_url
-        });
-}
+  if (!redisUrl) {
+    throw new Error("Missing 'redis_url' in environment variables");
+  }
+
+  const client = redis.createClient({
+    url: redisUrl,
+  });
+
+  client.on("error", (err) => {
+    console.error(err);
+  });
+
+  client.on("connect", () => {
+    console.log("Connected to Redis");
+  });
+
+  client.on("end", () => {
+    console.log("Redis connection ended");
+  });
+
+  process.on("SIGQUIT", () => {
+    client.quit(() => {
+      console.log("Redis client quit gracefully");
+    });
+  });
+
+  return client;
+};
 
 const client = redisClient();
-client.on("error",(err)=>{
-    console.log(err)
-})
-client.on("connect",()=>{
-    console.log("connected to redis")
-})
 
-client.on("end",()=>{
-    console.log("redis connection ended")
-})
-
-client.on("SIGQUIT",()=>{
-    client.quit()
-})
-
-
-module.exports= client
+module.exports = client;
